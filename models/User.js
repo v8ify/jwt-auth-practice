d65d0a839a1b38bce8b37c1de07a59dc23595646
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-07 17:05:12 +0530

    Added userschema with bcrypt configuration

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// Define userSchema
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email already in use"],
+  },
+  password: { type: String, required: [true, "Password is required"] },
+});
+
+// Hashing and salting password before saving to db
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Create model class from schema
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
